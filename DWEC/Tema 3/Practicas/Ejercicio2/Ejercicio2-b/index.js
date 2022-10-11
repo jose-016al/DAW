5e3cc132f8e0591286bb3667709126742751e5ac
jose-016al <jose_016@outlook.com>
2022-10-11 11:58:18 +0200

    Changes

--- DWEC/Tema 3/Practicas/Ejercicio2/Ejercicio2-b/index.js
+++ DWEC/Tema 3/Practicas/Ejercicio2/Ejercicio2-b/index.js
@@ -1,7 +1,10 @@
+var imagenes = document.images;
+console.log(imagenes);
 
-// var src = document.getElementById("container").src;
-
-for (let index = 0; index < document.images.length; index++) {
-    //var src = document.getElementById(document.images.index).src;
-    console.log(index);
+// no condigo mostrar los enlaces de las imagenes, desde el inspector si estan donde deberian 
+for (let index = 0; index < imagenes.length; index++) {
+    var src = document.getElementById(index).src;
+        let text = document.createElement("div");
+        text.textContent = "Ruta de la imagen " + index + ": " + src;
+        document.getElementById(index).appendChild(text);
 }
